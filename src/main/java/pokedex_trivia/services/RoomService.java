e1c0c156faf1075f3efb9b8a32d021d266e467e7
claudiocarvalhoo <cco2@cin.ufpe.br>
2019-12-09 21:25:47 -0300

    feat(RoomController): add post scores endpoint

--- src/main/java/pokedex_trivia/services/RoomService.java
+++ src/main/java/pokedex_trivia/services/RoomService.java
@@ -1,17 +1,24 @@
 package pokedex_trivia.services;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pokedex_trivia.facades.AlternativeFacade;
 import pokedex_trivia.facades.RoomFacade;
+import pokedex_trivia.facades.ScoreFacade;
 import pokedex_trivia.models.dtos.RoomDto;
 import pokedex_trivia.models.dtos.RoomSummaryDto;
+import pokedex_trivia.models.dtos.ScoreDto;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class RoomService {
   private RoomFacade roomFacade;
+  private AlternativeFacade alternativeFacade;
+  private ScoreFacade scoreFacade;
 
   public RoomDto getRoomById(Long id) {
     return roomFacade.getRoomById(id);
@@ -19,5 +26,12 @@ public class RoomService {
 
   public Set<RoomSummaryDto> getAllRoomSummaries() {
     return roomFacade.getAllRoomSummaries();
+  }
+
+  public void calculateScoreAndSave(Long roomId, String username, Set<UUID> alternativeIds) {
+    Map<UUID, Boolean> correctness = alternativeFacade.getCorrectnessOfAlternatives(alternativeIds);
+    Long numberOfCorrects = correctness.values().stream().filter(x -> x).count();
+    scoreFacade.addScoreToRoom(
+        roomId, ScoreDto.builder().username(username).score(numberOfCorrects).build());
   }
 }
