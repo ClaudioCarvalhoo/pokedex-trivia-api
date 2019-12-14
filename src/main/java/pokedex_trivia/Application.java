cd0cee5e4d6c3474e2a043564f65fb58a28179c8
claudiocarvalhoo <claudio.carvalho@inloco.com.br>
2019-12-13 22:54:53 -0300

    feat(Application): use config in application

--- src/main/java/pokedex_trivia/Application.java
+++ src/main/java/pokedex_trivia/Application.java
@@ -1,8 +1,12 @@
 package pokedex_trivia;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableAutoConfiguration
+@EnableCaching
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
